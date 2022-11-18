f94435210db19a5e424752fdafcf862a6df48cab
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2022-11-18 09:16:37 -0800

    factorial number

---
+++ stage.java
@@ -0,0 +1,11 @@
+class Factorial{
+    public static void main(String args[]){
+        int i,fact=1;
+        int number=5;
+        //it is the no to calculate factorial
+        for(i=1;<=number;i++){
+            fact=fact*i;
+        }
+        System.out.println("Factorial of "+number+"is:"+fact);
+    }
+}
